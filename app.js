fe7f3782004a4a6cf595c5162f673db27f96397d
Sami Singh <samuli@alajarvela.net>
2017-05-05 16:11:52 +0300

    added styling, fixed innerhtml issue

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
-let deadline = "05/05/2017 10:45";
-
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor(t / 1000 % 60);
-  var minutes = Math.floor(t / 1000 / 60 % 60);
-  var hours = Math.floor(t / (1000 * 60 * 60) % 24);
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
   var days = Math.floor(t / (1000 * 60 * 60 * 24));
   return {
-    total: t,
-    days: days,
-    hours: hours,
-    minutes: minutes,
-    seconds: seconds,
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
   };
 }
 
 function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
-  var timeinterval = setInterval(function() {
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
     var t = getTimeRemaining(endtime);
-    clock.innerHTML =
-      `days: $t.days <br>
-      hours: $t.hours <br>
-      minutes: $t.minutes <br>
-      seconds: $t.seconds`;
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
     if (t.total <= 0) {
       clearInterval(timeinterval);
     }
-  }, 1000);
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
 }
 
-initializeClock("clock", deadline);
+var deadline = '2017-09-02 12:00'
+initializeClock('clockdiv', deadline);
